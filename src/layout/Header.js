12b30b084a46797044ff3854b2cafc0fe599d31b
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-28 11:44:40 +0530

    added conditional rendering in navbar for signup ,signin and Logout

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -13,6 +13,12 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 const Header = () => {
+  const context = useContext(UserContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggler = () => setIsOpen(!isOpen);
+
   return (
     <Navbar color="info" light expand="md">
       <NavbarBrand>
@@ -20,19 +26,34 @@ const Header = () => {
           GitFire App
         </Link>
       </NavbarBrand>
-      <NavbarToggler />
-      <Collapse navbar />
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink className="text-white">SignIn</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="text-white">SignUp</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="text-white">Logout</NavLink>
-        </NavItem>
-      </Nav>
+      <NavbarText className="text-white">
+        {context.user?.email ? context.user.email : ""}
+      </NavbarText>
+      <NavbarToggler onClick={toggler} />
+      <Collapse navbar isOpen={isOpen}>
+        <Nav className="ml-auto" navbar>
+          {context.user ? (
+            <NavItem>
+              <NavLink tag={Link} to="/" className="text-white">
+                Logout
+              </NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavItem>
+                <NavLink tag={Link} to="/signin" className="text-white">
+                  SignIn
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/signup" className="text-white">
+                  SignUp
+                </NavLink>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };
